c5533335193f58c38c5fa701456ce2b59aec427d
matt <matt@matts-iMac.local>
2020-04-08 18:01:11 +0200

    jumping escape works as predicted.

--- Assets/Scripts/Move_transform.cs
+++ Assets/Scripts/Move_transform.cs
@@ -20,6 +20,7 @@ public class Move_transform : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool("finish_jump", true);
+
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
