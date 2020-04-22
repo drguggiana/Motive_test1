db4812068d3ecdba8e3b8424d961315dca516cb2
matt <matt@matts-iMac.local>
2020-04-22 14:52:37 +0200

    Rebuilt NavMeshes. Added child components to cricket for better collision detecting.

---
+++ Assets/Scripts/AdjustNormal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class AdjustNormal : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Wall" || c.gameObject.tag == "Floor")
+        {
+            gameObject.GetComponentInParent<WanderingAI_escape>().SetNormal(c);
+        }
+
+    }
+
+}
--- Assets/Scripts/Move_transform.cs
+++ Assets/Scripts/Move_transform.cs
@@ -12,15 +12,16 @@ public class Move_transform : StateMachineBehaviour
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.SetInteger("jump_walk_transition", Random.Range(0, 2));
         animator.SetBool("finish_jump", true);
-
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
