184bd114e882b31d2de9633fb8fb157c245cf4e3
matt <matt@matts-iMac.local>
2020-04-28 16:07:36 +0200

    New navmeshes

--- Assets/Scripts/AdjustNormal.cs
+++ Assets/Scripts/AdjustNormal.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-
 public class AdjustNormal : MonoBehaviour
 {
 
-    void OnTriggerEnter(Collider c)
-    {
-        if (c.gameObject.tag == "Wall" || c.gameObject.tag == "Floor")
-        {
-            gameObject.GetComponentInParent<WanderingAI_escape>().SetNormal(c);
-        }
+    //void OnTriggerEnter(Collider c)
+    //{
+    //    if (c.gameObject.tag == "Wall" || c.gameObject.tag == "Floor")
+    //    {
+    //        gameObject.GetComponentInParent<WanderingAI_escape>().SetNormal(c);
+    //    }
 
-    }
+    //}
 
 }
