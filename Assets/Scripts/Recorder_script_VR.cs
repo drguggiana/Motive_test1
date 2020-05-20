0573e3bd0e3b911f2b62fd9f6a04203489b6c304
matt <matt@matts-iMac.local>
2020-05-20 12:05:33 +0200

    VR cricket data saving implemented

--- Assets/Scripts/Recorder_script_VR.cs
+++ Assets/Scripts/Recorder_script_VR.cs
@@ -3,64 +3,138 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
-//using System.Collections;
 
-public class Recorder_script_VR : MonoBehaviour {
 
-	public OptitrackStreamingClient StreamingClient;
-	public Int32 RigidBodyId;
-	private OptitrackHiResTimer.Timestamp reference;
+[ExecuteInEditMode]
+public class Recorder_script_VR : MonoBehaviour
+{
 
-	private Vector3 Mouse_Position;
-	private Vector3 Mouse_Orientation;
-	private Vector3 Cricket;
-	private GameObject CricketObj;
-	private float Time_stamp;
-	private StreamWriter writer;
+    // Streaming client
+    public OptitrackStreamingClient StreamingClient;
 
-	// Use this for initialization
-	void Start () {
-		// Get the reference for the timestamp
-		reference = OptitrackHiResTimer.Now ();
-		// Set up the camera (so it doesn't clip objects too close to the mouse)
-		Camera cam = GetComponentInChildren<Camera>();
-		cam.nearClipPlane = 0.000001f;
-		// Find the cricket object
-		CricketObj = GameObject.Find ("Cricket");
-		// Set the writer
-		writer = new StreamWriter(Paths.recording_path, true);
+    // Variables for mouse position
+    private Vector3 Mouse_Position;
+    private Vector3 Mouse_Orientation;
 
-	}
+    // Variables for cricket transforms and states
+    private GameObject CricketObj;
+    private Vector3 Cricket_Position;
+    private Vector3 Cricket_Orientation;
+    private int state;
+    private float speed;
 
-	// Update is called once per frame
-	void Update () {
+    // OSC communication client
+    public OSC osc;
 
-		// Process the mouse position as the other scripts
-		OptitrackRigidBodyState rbState = StreamingClient.GetLatestRigidBodyState(RigidBodyId);
-		if (rbState != null)
-		{
-			Mouse_Position = rbState.Pose.Position;
-			this.transform.localPosition = Mouse_Position;
+    // Variables for tracking square
+    public GameObject tracking_square;
+    private float color_factor = 0.0f;
+    private Color new_color;
 
-			this.transform.localRotation = rbState.Pose.Orientation;
-			Mouse_Orientation = this.transform.eulerAngles;
-			Time_stamp = rbState.DeliveryTimestamp.SecondsSince(reference);
+    // Rigid body ID for mouse tracking
+    public Int32 RigidBodyId;
 
-		}
-		// Get the VR cricket position
-		Cricket = CricketObj.transform.position;
-		// Write the mouse and VR cricket info
-		writer.WriteLine(string.Concat(Time_stamp.ToString(), ',', Mouse_Position.x.ToString(), ',', Mouse_Position.y.ToString(), ',', Mouse_Position.z.ToString(), ',',
-			Mouse_Orientation.x.ToString(), ',', Mouse_Orientation.y.ToString(), ',', Mouse_Orientation.z.ToString(), ',',
-			Cricket.x.ToString(), ',', Cricket.y.ToString(), ',', Cricket.z.ToString()));
+    // Timer
+    private OptitrackHiResTimer.Timestamp reference;
+    private float Time_stamp;
 
-	}
+    // Writer for saving data
+    private StreamWriter writer;
 
-	void OnReceiveStop(OscMessage message){
-		// Close the writer
-		writer.Close();
-		// Kill the application
-		Application.Quit ();
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the reference timer
+        reference = OptitrackHiResTimer.Now();
+
+        // set the OSC communication
+        osc.SetAddressHandler("/Close", OnReceiveStop);
+
+        // Set up the camera (so it doesn't clip objects too close to the mouse)
+        Camera cam = GetComponentInChildren<Camera>();
+        cam.nearClipPlane = 0.000001f;
+
+        // Set the writer
+        writer = new StreamWriter(Paths.recording_path, true);
+
+        // Find the cricket object
+        CricketObj = GameObject.Find("Cricket");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // --- Handle the tracking square --- //
+
+        // create the color for the square
+        new_color = new Color(color_factor, color_factor, color_factor, 1f);
+        // put it on the square
+        tracking_square.GetComponent<Renderer>().material.SetColor("_Color", new_color);
+
+        // Define the color for the next iteration (switch it)
+        if (color_factor > 0.0f)
+        {
+            color_factor = 0.0f;
+        }
+        else
+        {
+            color_factor = 1.0f;
+        }
+
+
+        // --- Handle mouse and cricket data --- //
+        // This works for a single VR cricket
+
+        // Process the mouse position as the other scripts
+        OptitrackRigidBodyState rbState = StreamingClient.GetLatestRigidBodyState(RigidBodyId);
+        if (rbState != null)
+        {
+            // get the position of the mouse RB
+            Mouse_Position = rbState.Pose.Position;
+            // change the transform position of the game object
+            this.transform.localPosition = Mouse_Position;
+            // also change its rotation
+            this.transform.localRotation = rbState.Pose.Orientation;
+            // turn the angles into Euler (for later printing)
+            Mouse_Orientation = this.transform.eulerAngles;
+            // get the timestamp 
+            Time_stamp = rbState.DeliveryTimestamp.SecondsSince(reference);
+
+        }
+
+        // Get the VR cricket position and orientation
+        Cricket_Position = CricketObj.transform.position;
+        Cricket_Orientation = CricketObj.transform.eulerAngles;
+
+        // Get the VR cricket speed and current motion state
+        speed = CricketObj.GetComponent<Animator>().GetFloat("speed"); ;
+        state = CricketObj.GetComponent<Animator>().GetInteger("state_selector");
+
+
+        // --- Data Saving --- //
+
+        // Write the mouse and VR cricket info
+        writer.WriteLine(
+            string.Concat(
+                Time_stamp.ToString(), ',',
+                Mouse_Position.x.ToString(), ',', Mouse_Position.y.ToString(), ',', Mouse_Position.z.ToString(), ',',
+                Mouse_Orientation.x.ToString(), ',', Mouse_Orientation.y.ToString(), ',', Mouse_Orientation.z.ToString(), ',',
+                Cricket_Position.x.ToString(), ',', Cricket_Position.y.ToString(), ',', Cricket_Position.z.ToString(), ',',
+                Cricket_Orientation.x.ToString(), ',', Cricket_Orientation.y.ToString(), ',', Cricket_Orientation.z.ToString(), ',',
+                speed.ToString(), ',', state.ToString()
+                )
+            );
+
+    }
+
+    void OnReceiveStop(OscMessage message)
+    {
+        // Close the writer
+        writer.Close();
+        // Kill the application
+        Application.Quit();
+    }
 
 }
